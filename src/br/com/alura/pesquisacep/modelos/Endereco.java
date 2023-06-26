42221b01c7beaaa9ccf4855cdd3a992c6f4c4a3a
Robson Paulo <65135918+rabisquim@users.noreply.github.com>
2023-06-26 16:43:21 -0300

    Update Endereco.java
    Atualização Classe Endereco

--- src/br/com/alura/pesquisacep/modelos/Endereco.java
+++ src/br/com/alura/pesquisacep/modelos/Endereco.java
@@ -1,2 +1,35 @@
-package br.com.alura.pesquisacep.br.com.alura.pesquisacep.modelos;public class Endereco {
+package br.com.alura.pesquisacep.modelos;
+
+public class Endereco {
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    @Override
+    public String toString() {
+        return "CEP: " + cep +
+                "\nLogradouro: " + logradouro +
+                "\nComplemento: " + complemento +
+                "\nBairro: " + bairro +
+                "\nLocalidade: " + localidade +
+                "\nUF: " + uf;
+    }
+
+
 }
