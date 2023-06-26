8acb7e399bd2f4e67917b84911f83c9a5a520569
Robson Paulo <65135918+rabisquim@users.noreply.github.com>
2023-06-26 16:45:13 -0300

    Update Principal.java
    Atualização Classe Principal

--- src/br/com/alura/pesquisacep/principal/Principal.java
+++ src/br/com/alura/pesquisacep/principal/Principal.java
@@ -1,2 +1,11 @@
-package principal;public class Principal {
+package br.com.alura.pesquisacep.principal;
+import br.com.alura.pesquisacep.modelos.Menu;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+        menu.exibeMenu();
+
+    }
 }
