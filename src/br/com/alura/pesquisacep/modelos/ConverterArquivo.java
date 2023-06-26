85ac0fb7f78929edc1d906075f4614a312af23af
Robson Paulo <65135918+rabisquim@users.noreply.github.com>
2023-06-26 16:42:48 -0300

    Update ConverterArquivo.java
    Atualização Classe ConverterArquivo

--- src/br/com/alura/pesquisacep/modelos/ConverterArquivo.java
+++ src/br/com/alura/pesquisacep/modelos/ConverterArquivo.java
@@ -1,11 +1,35 @@
 package br.com.alura.pesquisacep.modelos;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
-public class GerarJson {
+public class ConverterArquivo {
 
-        FileWriter escrita = new FileWriter("Filmes.json");
-        escrita.write(gson.toJson(enderecos));
-        escrita.close();
+        //private Gson gson;
+        private ListaEnderecos lista;
+
+        public ConverterArquivo (ListaEnderecos lista) {
+                //this.gson = new Gson();
+                this.lista = lista;
+        }
+        public void exportar()  {
+
+                List<Endereco> enderecos = lista.getEnderecos();
+
+                try {
+                        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                        FileWriter arquivo = new FileWriter("listagem_enderecos.json");
+                        arquivo.write(gson.toJson(enderecos));
+                        arquivo.close();
+                        System.out.println("Arquivo de listagem de endereço exportado com sucesso!");
+                } catch (IOException e) {
+                        System.out.println("Erro ao exportar arquivo de listagem de endereços: " + e.getMessage());
+                }
+
+        }
+
 }
